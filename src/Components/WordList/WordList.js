66162e330a5dbaa1abb6c01c08089491dc19fd7f
ShoshBeer <shoshanabeer@gmail.com>
2023-01-24 10:52:53 -0500

    createWordList returns related words given a word

--- src/Components/WordList/WordList.js
+++ src/Components/WordList/WordList.js
@@ -1,27 +1,45 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 
 export function WordList(props) {
+  const [targetWord, setTargetWord] = useState('example');
+  const [words, setWords] = useState([]);
+  
+  const selectRandomWord = (language = 'en', difficulty = 'basic') => {
+    //provide a random word
+    //when fully implemented, it will need to take parameter for the language and the difficulty of the word
+    //difficulty would relate to both length/rarity, but also to concrete vs abstract concepts
+  }
 
-    const selectRandomWord = (language = 'en', difficulty = 'basic') => {
-        //provide a random word
-        //when fully implemented, it will need to take parameter for the language and the difficulty of the word
-        //difficulty would relate to both length/rarity, but also to concrete vs abstract concepts
+  useEffect(() => {
+    const createWordList = async (targetWord, numOfWords = 5) => {
+      const responseTrg = await fetch(`https://api.datamuse.com/words?rel_trg=${targetWord}&max=${numOfWords}`);
+      const responseSyn = await fetch(`https://api.datamuse.com/words?rel_syn=${targetWord}&max=${numOfWords}`);
+      if (responseTrg.ok) {
+        const relatedWords = await responseTrg.json();
+        setWords(relatedWords);
+      }
+      if (responseSyn.ok) {
+        const synonyms = await responseSyn.json();
+        setWords(listOfWords => listOfWords.concat(synonyms));
+      }
+      //creates a list of words related to the target word
+      //words should be restricted to same difficulty or easier relative to the target word
+      //words should be in the target language
+      //can modify number of words
+      //later feature: add vocabulary word(s) manually and create cards for them (useful for school settings)
     }
+    createWordList(targetWord);
+  }, [targetWord])
 
-    const targetWord = selectRandomWord();
-
-    const createWordList = (targetWord, numOfWords = 5) => {
-        //creates a list of words related to the target word
-        //words should be restricted to same difficulty or easier relative to the target word
-        //words should be in the target language
-        //can modify number of words
-        //later feature: add vocabulary word(s) manually and create cards for them (useful for school settings)
-    }
-
-    return (
-        <div className="word-list">
-            {listOfWords.map(word, index => <Word word={word} key={index} />)}
-            {/* Word components rendered instead of listing the words here for styling and for potential to add hover over definitions */}
-        </div>
-    )
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{targetWord}</Card.Title>
+        {words.map((word, index) => <Card.Text key={index}>{word.word}</Card.Text>)}
+        {/* want to add hover over definitions */}
+      </Card.Body>
+    </Card>
+  )
 }
