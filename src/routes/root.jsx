3b1b23697e195fe80eb0cdb4866bcb215e99ca62
ShoshBeer <shoshanabeer@gmail.com>
2023-03-04 15:22:22 -0500

    add footer with CC license to index.html

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -8,7 +8,7 @@ export function Root() {
       <Row>
         <Col>
           <nav>
-            <h1>Talking in Circles</h1>
+            <h1>Talking Circles</h1>
             <input className="menu-btn" type="checkbox" id="menu-btn" />
             <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
             <ul className="menu">
@@ -25,6 +25,15 @@ export function Root() {
           <Outlet />
         </Col>
       </Row>
+      {/* <Row>
+        <footer>
+          <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+            <img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+          </a>
+          <br />
+          <span {...{'xmlns:dct': "http://purl.org/dc/terms/"}} property="dct:title">Talking Circles</span> by <span {...{'xmlns:cc': "http://creativecommons.org/ns#"}} property="cc:attributionName">Shoshana Beer</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+        </footer>
+      </Row> */}
     </Container>
   )
 }
